c470beb530a91acd710276c77fc44ab017bdc0db
Erick Mayson <ferreiraerick0101@gmail.com>
2021-01-06 20:44:50 -0300

    Order page

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,7 +1,8 @@
 import './styles.css';
 import {ReactComponent as MainImage} from "./main.svg";
 import Footer from '../Footer';
-function Navbar() {
+import { Link } from 'react-router-dom';
+function Home() {
     return (
        <>
        <div className="home-container">
@@ -14,9 +15,9 @@ function Navbar() {
                 Escolha o seu pedido e em poucos minutos <br /> 
                 levaremos na sua porta
                 </h3>
-                <a href="orders" className="home-btn-order">
+                <Link to="/orders" className="home-btn-order">
                     FAZER PEDIDO
-                </a>
+                </Link>
             </div>      
             <div className="home-image">
                 <MainImage />  
@@ -28,4 +29,4 @@ function Navbar() {
     )
 }
 
-export default Navbar;
+export default Home;
