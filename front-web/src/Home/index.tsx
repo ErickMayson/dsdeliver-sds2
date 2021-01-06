ce679d78285fd2f9e90c46c435afcee03bff426f
Erick Mayson <ferreiraerick0101@gmail.com>
2021-01-06 14:56:22 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,27 +1,30 @@
 import './styles.css';
-import {ReactComponent as Logo} from "./logo.svg";
 import {ReactComponent as MainImage} from "./main.svg";
+import Footer from '../Footer';
 function Navbar() {
     return (
+       <>
        <div className="home-container">
            <div className="home-content">
                <div className="home-actions">
                 <h1 className="home-title">
-                Faça seu pedido <br /> que entregamos pra você!!!
+                Faça seu pedido <br /> que entregamos <br /> pra você!!!
                 </h1>
                 <h3 className="home-subtitle">
-                Escolha o seu pedido e em poucos minutos <br /> levaremos na sua porta
+                Escolha o seu pedido e em poucos minutos <br /> 
+                levaremos na sua porta
                 </h3>
-               </div>
                 <a href="orders" className="home-btn-order">
                     FAZER PEDIDO
                 </a>
-
-           </div>
-           <div className="home-image">
-            <MainImage />
-           </div>
-       </div>
+            </div>      
+            <div className="home-image">
+                <MainImage />  
+            </div>   
+        </div>
+        </div>               
+       <Footer />
+       </>
     )
 }
 
