fce5ca726f138c318483ab16bd6c26787740db39
Erick Mayson <ferreiraerick0101@gmail.com>
2021-01-10 22:28:16 -0300

    front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { OrderLocationData, OrderPayload } from "./Orders/types";
 
-const API_URL ="http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
@@ -14,5 +14,5 @@ export function fetchLocalMapBox(local: string) {
 }
 
 export function saveOrder(payload: OrderPayload) {
-    axios.post(`${API_URL}/orders`, payload);
+   return axios.post(`${API_URL}/orders`, payload);
 }   
