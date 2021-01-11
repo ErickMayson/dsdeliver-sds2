cb37e08f70f9531d39563c3c70f11b15152271ae
Erick Mayson <ferreiraerick0101@gmail.com>
2021-01-10 22:12:06 -0300

    Finish Front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Toast } from 'react-toastify/dist/components';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Routes from './Routes';
 
 function App() {
   return (
-    <div>
+    <>
       <Routes />
-    </div>
+      <ToastContainer/>
+    </>
   );
 }
 
--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,3 +1,6 @@
+import OrderLocation from "./OrderLocation"
+import ProductCard from "./ProductCard"
+
 export type Product = {
     id: number;
     name: string;
@@ -6,8 +9,18 @@ export type Product = {
     imageUri: string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
+
+
